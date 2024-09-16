5f7215101fb1de1c50ec1702e58fd71cbdb83b0d
nehasheikh4 <nehaghulammohammed@gmail.com>
2024-09-16 22:27:45 +0500

    first commit

---
+++ js/data.js
@@ -0,0 +1,53 @@
+var product = [
+  {
+    id: 0,
+    title: "",
+    price: "",
+    brand: "",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png",
+  },
+  {
+    id: 1,
+    title: "Wall Painting",
+    price: 12.26,
+    brand: "Wall Decor",
+    image: "images/1024-x-1024-1_0a654e01-4a5e-4b86-8e99-8c2caf3d44e3_540x.jpg",
+  },
+  {
+    id: 2,
+    title: "Mirror Art",
+    price: 20.85,
+    brand: "Wall Decor",
+    image: "images/1660535743f0b2c7702631e0c1847e64bc40f8633f.jpg",
+  },
+  {
+    id: 3,
+    title: "Wall Mouted Animal",
+    price: 12.92,
+    brand: "Wall Decor",
+    image: "images/5o.jpg",
+  },
+  {
+    id: 4,
+    title: "Metal Wall Art",
+    price: 60.88,
+    brand: "Wall Decor",
+    image: "./images/october-01-2-600x600.jpg",
+  },
+  {
+    id: 5,
+    title: "Wallpaper",
+    price: 45.5,
+    brand: "Wall Decor",
+    image: "./images/wall paint.jpg",
+  },
+  {
+    id: 6,
+    title: "Photo Frame Art",
+    price: 70.33,
+    brand: "Wall Decor",
+    image: "./images/Colorful-Poster-Vertical_02-Mockup_e9cd0f93-f3dc-4fac-92b7-d6cc71df9257_350x.png",
+  },
+ 
+];
